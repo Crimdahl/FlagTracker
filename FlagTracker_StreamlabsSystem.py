8ea568e274b7bf2b59e228db60cfd134d87e18df
Crimdahl <crimdahl@outlook.com>
2021-06-03 05:56:39 -0400

    Update FlagTracker_StreamlabsSystem.py
    Updated version number.

--- FlagTracker_StreamlabsSystem.py
+++ FlagTracker_StreamlabsSystem.py
@@ -13,7 +13,7 @@ ScriptName = "FlagTracker"
 Website = "https://www.twitch.tv/Crimdahl"
 Description = "Tracks User Flag Redemptions by writing to json file."
 Creator = "Crimdahl"
-Version = "1.2.0-Beta"
+Version = "1.2.2-Beta"
 
 #   Define Global Variables <Required>
 ScriptPath = os.path.dirname(__file__)
